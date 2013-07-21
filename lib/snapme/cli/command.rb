acd48d484462f9ac2a60ec61a40c11d40c5972a2
Jay Hayes <ur@iamvery.com>
2013-07-20 19:55:30 -0500

    Print message and quit if auth_token isn't set in environment

--- lib/snapme/cli/command.rb
+++ lib/snapme/cli/command.rb
@@ -2,9 +2,18 @@ module Snapme
   module CLI
     class Command
       def self.start(args)
-        options = Options.parse(args)
-        Process.daemon(true) if options.daemon
-        Snapper.new(options.host, options.interval).run
+        options    = Options.parse(args)
+        auth_token = ENV['SNAPME_AUTH_TOKEN']
+
+        if auth_token
+          Process.daemon(true) if options.daemon
+          Snapper.new(options.host, options.interval, auth_token).run
+        else
+          puts <<-MSG
+  Please set SNAPME_AUTH_TOKEN in your environment. You can get your
+  auth token from http://snapme.herokuapp.com
+          MSG
+        end
       end
     end
   end
